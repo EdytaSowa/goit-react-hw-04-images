384f9c82081843fd4fbdc26a593140ec18b2321e
EdytaSowa <113289486+EdytaSowa@users.noreply.github.com>
2023-06-18 13:47:32 +0200

    loader  - functional component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,25 +1,23 @@
 import { Bars } from 'react-loader-spinner';
 import css from './Loader.module.css'
-import {Component} from 'react';
 
 
+export const Loader = () => {
+  return (
+    <div className={css.Loader}>
+   <Bars
+      height="300"
+      width="300"
+      color="#3f51b5"
+      ariaLabel="bars-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+    </div>
+  );
+}
 
-export class Loader extends Component {
-    render() {
-      return (
-        <div className={css.Loader}>
-       <Bars
-          height="300"
-          width="300"
-          color="#3f51b5"
-          ariaLabel="bars-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-          visible={true}
-        />
-        </div>
-      );
-    }
-  }
 
+  
   
